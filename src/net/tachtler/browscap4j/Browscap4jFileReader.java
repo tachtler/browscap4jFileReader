eb71b28705f7a7d56fdc238b9d185e3b10fe8149
Klaus Tachtler <klaus@tachtler.net>
2017-05-09 10:07:34 +0200

    Redesign Browscap4jFileReader.java
    Complete redesign of the Browscap4jFileReader.
    
    Main changes are:
    - Reading the file without using opencsv.jar any more
    - Reading the file and generate a Browscap4jDataBean with Browscap4jMap and Browscap4jString instead of Browscap4jMap only
    - Browscap4jMap has the compiled regular expression pattern as key and the only Browscap4jPositionBean as value
    - Browscap4jString contains all the lines read from file as one concatenated string
    - Access to the data are done by sub string against the Browscap4jMap with position from Browscap4jPositionBean
    
    The access time are 20% faster than on version 1.0 on low memory and the memory consumption was 50% less than on version 1.0 after initialisation

--- src/net/tachtler/browscap4j/Browscap4jFileReader.java
+++ src/net/tachtler/browscap4j/Browscap4jFileReader.java
@@ -1,29 +1,19 @@
 package net.tachtler.browscap4j;
 
 import java.io.File;
-
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
 import org.apache.commons.lang3.CharUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import com.opencsv.CSVParserBuilder;
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
 
 /*******************************************************************************
  * Browscap4jFileReader read the User-Agent-String capabilities from
@@ -45,14 +35,6 @@ public class Browscap4jFileReader {
 	private static Logger log = LogManager.getLogger();
 
 	/**
-	 * Map with regular expression pattern as key from the original
-	 * userAgentString and a browscap4jBean as value object.
-	 */
-	private static Map<Pattern, Browscap4jBean> browscap4jMap = null;
-
-	private static Browscap4jBean browscap4jBean = null;
-
-	/**
 	 * Constructor.
 	 */
 	public Browscap4jFileReader() {
@@ -60,20 +42,44 @@ public class Browscap4jFileReader {
 	}
 
 	/**
-	 * Initialize the browsecap4jMap with regular expression pattern as key from
-	 * the original userAgentString and a browscap4jBean from csvFile as value
-	 * object.
+	 * Initialize the Browscap4jDataBean.
 	 * 
 	 * @param csvFile
-	 * @return browsecap4jMap<Pattern, Browscap4jBean>
+	 * @return Browscap4jDataBean
 	 * @throws IllegalStateException
 	 * @throws FileNotFoundException
 	 * @throws IOException
 	 */
-	public static Map<Pattern, Browscap4jBean> initBrowscap4jMap(File csvFile)
+	public static Browscap4jDataBean initBrowscap4jData(File csvFile)
 			throws IllegalStateException, FileNotFoundException, IOException {
 
 		log.debug("*csvFile                                : " + csvFile);
+
+		/**
+		 * Browscap4jDataBean with the LinkedHashMap<Pattern,
+		 * Browscap4jPositionBean> browscap4jMap and the String browscap4jString
+		 * as data.
+		 */
+		Browscap4jDataBean browscap4jDataBean = new Browscap4jDataBean();
+
+		/**
+		 * LinkedHashMap with regular expression pattern as key from the
+		 * original userAgentString and Browscap4jPositionBean integer variables
+		 * as value object by determining the offset and the length for each
+		 * line.
+		 */
+		LinkedHashMap<Pattern, Browscap4jPositionBean> browscap4jMap = new LinkedHashMap<Pattern, Browscap4jPositionBean>();
+
+		/**
+		 * Generate browscap4jString with all fields filled up with data from
+		 * browscap.csv to a single String.
+		 */
+		String browscap4jString = null;
+
+		/**
+		 * StringBuilder for fast concatenation.
+		 */
+		StringBuilder stringBuilder = new StringBuilder();
 
 		/*
 		 * Check if csvFile is null.
@@ -83,145 +89,91 @@ public class Browscap4jFileReader {
 		}
 
 		/*
-		 * Generate List<Browscap4jBean> listOfBrowscap4jBeans with all fields
-		 * filled up with data from browscap.csv to a list of Browscap4jBeans
-		 * using the OpenCSV from http://opencsv.sourceforge.net/
+		 * Iterate over the csvFile - browscap.csv with all the data and
+		 * generate a string with all the lines concatenated. Generate a regular
+		 * expression pattern from the first column of the csvFile -
+		 * browscap.csv as key and calculate the offset and the length for every
+		 * single line inside the concatenated string as Browscap4jPositionBean
+		 * as value.
 		 */
-		List<Browscap4jBean> listOfBrowscap4jBeans = new ArrayList<Browscap4jBean>();
+		LineIterator lineIterator = FileUtils.lineIterator(csvFile, "UTF-8");
 
 		try {
-			/*
-			 * Use comma as separator, quote as quotechar and skip the first
-			 * three lines from csvFile - browscap.csv.
-			 */
-			final CSVReader csvReader = new CSVReaderBuilder(new FileReader(csvFile)).withSkipLines(3)
-					.withCSVParser(new CSVParserBuilder().withSeparator(',').withQuoteChar('\"').build()).build();
+			int offset = 0;
+			String[] col = null;
 
-			String[] col;
-			while ((col = csvReader.readNext()) != null) {
+			while (lineIterator.hasNext()) {
+				String line = lineIterator.nextLine();
 
-				/*
-				 * Generate for every entry a new Browscap4jBean.
-				 */
-				Browscap4jBean browscap4jBean = new Browscap4jBean();
+				Browscap4jPositionBean browscap4jPositionBean = new Browscap4jPositionBean(offset,
+						offset + line.length());
 
-				/*
-				 * Fill up the former generated browscap4jBean with the fields
-				 * from csvFile - browscap.csv.
-				 */
-				browscap4jBean.setPropertyName(col[0]);
-				browscap4jBean.setMasterParent(col[1]);
-				browscap4jBean.setLiteMode(col[2]);
-				browscap4jBean.setParent(col[3]);
-				browscap4jBean.setComment(col[4]);
-				browscap4jBean.setBrowser(col[5]);
-				browscap4jBean.setBrowserType(col[6]);
-				browscap4jBean.setBrowserBits(col[7]);
-				browscap4jBean.setBrowserMaker(col[8]);
-				browscap4jBean.setBrowserModus(col[9]);
-				browscap4jBean.setVersion(col[10]);
-				browscap4jBean.setMajorVersion(col[11]);
-				browscap4jBean.setMinorVersion(col[12]);
-				browscap4jBean.setPlatform(col[13]);
-				browscap4jBean.setPlatformVersion(col[14]);
-				browscap4jBean.setPlatformDescription(col[15]);
-				browscap4jBean.setPlatformBits(col[16]);
-				browscap4jBean.setPlatformMaker(col[17]);
-				browscap4jBean.setAlpha(col[18]);
-				browscap4jBean.setBeta(col[19]);
-				browscap4jBean.setWin16(col[20]);
-				browscap4jBean.setWin32(col[21]);
-				browscap4jBean.setWin64(col[22]);
-				browscap4jBean.setFrames(col[23]);
-				browscap4jBean.setiFrames(col[24]);
-				browscap4jBean.setTables(col[25]);
-				browscap4jBean.setCookies(col[26]);
-				browscap4jBean.setBackgroundSounds(col[27]);
-				browscap4jBean.setJavaScript(col[28]);
-				browscap4jBean.setVbScript(col[29]);
-				browscap4jBean.setJavaApplets(col[30]);
-				browscap4jBean.setActiveXControls(col[31]);
-				browscap4jBean.setIsMobileDevice(col[32]);
-				browscap4jBean.setIsTablet(col[33]);
-				browscap4jBean.setIsSyndicationReader(col[34]);
-				browscap4jBean.setCrawler(col[35]);
-				browscap4jBean.setIsFake(col[36]);
-				browscap4jBean.setIsAnonymized(col[37]);
-				browscap4jBean.setIsModified(col[38]);
-				browscap4jBean.setCssVersion(col[39]);
-				browscap4jBean.setAolVersion(col[40]);
-				browscap4jBean.setDeviceName(col[41]);
-				browscap4jBean.setDeviceMaker(col[42]);
-				browscap4jBean.setDeviceType(col[43]);
-				browscap4jBean.setDevicePointingMethod(col[44]);
-				browscap4jBean.setDeviceCodeName(col[45]);
-				browscap4jBean.setDeviceBrandName(col[46]);
-				browscap4jBean.setRenderingEngineName(col[47]);
-				browscap4jBean.setRenderingEngineVersion(col[48]);
-				browscap4jBean.setRenderingEngineDescription(col[49]);
-				browscap4jBean.setRenderingEngineMaker(col[50]);
+				offset += line.length();
 
-				/*
-				 * Add the former generated Browscap4jBean to the
-				 * listOfBrowscap4jBeans.
-				 */
-				listOfBrowscap4jBeans.add(browscap4jBean);
+				col = line.split("\",\"");
+
+				browscap4jMap.put(Pattern.compile(convertToRegex(col[0].substring(1))), browscap4jPositionBean);
+
+				stringBuilder.append(line);
 			}
-		} catch (IOException e) {
-			log.error("IOException                           : " + e);
-			e.printStackTrace();
+		} finally {
+			LineIterator.closeQuietly(lineIterator);
 		}
 
 		/*
-		 * Convert the List<Browscap4jBean> listOfBrowscap4jBeans with all the
-		 * fields filled up from csvFile - browscap.csv to a list of
-		 * Browscap4jBeans into a LinkedHashMap using the Java 8 (1.8) stream
-		 * capabilities.
-		 * 
-		 * Maybe:
-		 * 
-		 * .sorted(Comparator.comparing(Browscap4jBean::getPropertyName).
-		 * reversed())
-		 * 
+		 * Generate the String browscap4jString from StringBuilder
+		 * stringBuilder.
 		 */
-		browscap4jMap = listOfBrowscap4jBeans.stream().parallel()
-				.collect(Collectors.toMap(
-						browscap4jBean -> Pattern.compile(convertToRegex(browscap4jBean.getPropertyName()),
-								Pattern.CASE_INSENSITIVE),
-						browscap4jBean -> browscap4jBean, (p1, p2) -> p1, LinkedHashMap::new));
+		browscap4jString = stringBuilder.toString();
 
 		/*
-		 * Debug NOT the Map, because of too much entry's, only the class should
-		 * be printed out in debug mode, to see if the browscap4jMap is NOT
-		 * null.
+		 * Debug NOT the Map and the String, because of too much entry's, only
+		 * the class should be printed out in debug mode, to see if the
+		 * browscap4jMap and the browscap4jString are NOT null.
 		 */
 		log.debug("*browscap4jMap                          : " + browscap4jMap.getClass());
+		log.debug("*browscap4jString                       : " + browscap4jString.getClass());
 
-		return browscap4jMap;
+		browscap4jDataBean.setBrowscap4jMap(browscap4jMap);
+		browscap4jDataBean.setBrowscap4jString(browscap4jString);
+
+		return browscap4jDataBean;
 	}
 
 	/**
 	 * Determining the Browscap4jCapabilities with the userAgentString from
-	 * browsecap4jMap and return the result in a browscap4jBean.
+	 * Browscap4jDataBean and return the result in a browscap4jBean.
 	 * 
-	 * @param browsecap4jMap<Pattern,
-	 *            Browscap4jBean>
+	 * @param browscap4jDataBean
 	 * @param userAgentString
-	 * @return browscap4jBean
+	 * @return Browscap4jBean
 	 */
-	public static Browscap4jBean determineBrowscap4jCapabilities(Map<Pattern, Browscap4jBean> browsecap4jMap,
+	public static Browscap4jBean determineBrowscap4jCapabilities(Browscap4jDataBean browscap4jDataBean,
 			String userAgentString) {
 
 		/*
-		 * Debug NOT the Map, because of too much entry's, only the class should
-		 * be printed out in debug mode, to see if the browscap4jMap is NOT
-		 * null.
+		 * Debug NOT the browscap4jDataBean, because of too much entry's, only
+		 * the class should be printed out in debug mode, to see if the
+		 * browscap4jDataBean is NOT null.
 		 */
-		log.debug("*browsecap4jMap                         : " + browsecap4jMap.getClass());
+		log.debug("*browscap4jDataBean                     : " + browscap4jDataBean.getClass());
+
+		/*
+		 * Debug NOT the Map and the String, because of too much entry's, only
+		 * the class should be printed out in debug mode, to see if the
+		 * browscap4jMap and the browscap4jString are NOT null.
+		 */
+		log.debug("*browscap4jDataBean.getBrowscap4jMap()  : " + browscap4jDataBean.getBrowscap4jMap().getClass());
+		log.debug("*browscap4jString                       : " + browscap4jDataBean.getBrowscap4jString().getClass());
 
 		log.debug("*userAgentString                        : " + userAgentString);
 
-		/*
+		/**
+		 * Initialize target Browscap4jBean for the result.
+		 */
+		Browscap4jBean browscap4jBean = null;
+
+		/**
 		 * Instance browscap4jBean and initialize all values to "" (empty)
 		 * strings.
 		 */
@@ -245,24 +197,87 @@ public class Browscap4jFileReader {
 		 * Java 8 (1.8) parallel stream possibilities while using the stream and
 		 * the userAgentStringRegEx.
 		 */
-		final Optional<Entry<Pattern, Browscap4jBean>> browsecap4jMapEntry = browsecap4jMap.entrySet().stream()
-				.parallel().filter(entry -> {
+		final Optional<Entry<Pattern, Browscap4jPositionBean>> browsecap4jMapEntry = browscap4jDataBean
+				.getBrowscap4jMap().entrySet().stream().parallel().filter(entry -> {
 					final Matcher matcher = entry.getKey().matcher(userAgentStringRegEx);
 					return matcher.matches();
 				}).findFirst();
 
 		/*
 		 * Return the search result browsecap4jMapEntry if one was present. If
-		 * true, return the browscap4jBean value from browscap4jMap. If false
-		 * return browscap4jBean with empty values and the search field's
+		 * true, generate a browscap4jBean from the browscap4jString using the
+		 * Browscap4jPositionBean position values from browscap4jMap . If false
+		 * generate a browscap4jBean with empty values and the search fields
 		 * userAgentString and userAgentStringRegEx.
 		 */
 		if (browsecap4jMapEntry.isPresent()) {
-			browscap4jBean = browsecap4jMapEntry.get().getValue();
+
+			String[] result = browscap4jDataBean.getBrowscap4jString()
+					.substring(browsecap4jMapEntry.get().getValue().getOffset(),
+							browsecap4jMapEntry.get().getValue().getLength())
+					.split("\",\"");
+
+			/*
+			 * Fill up the former generated browscap4jBean with the fields from
+			 * csvFile - browscap.csv.
+			 */
+			browscap4jBean.setPropertyName(result[0].substring(1));
+			browscap4jBean.setMasterParent(result[1]);
+			browscap4jBean.setLiteMode(result[2]);
+			browscap4jBean.setParent(result[3]);
+			browscap4jBean.setComment(result[4]);
+			browscap4jBean.setBrowser(result[5]);
+			browscap4jBean.setBrowserType(result[6]);
+			browscap4jBean.setBrowserBits(result[7]);
+			browscap4jBean.setBrowserMaker(result[8]);
+			browscap4jBean.setBrowserModus(result[9]);
+			browscap4jBean.setVersion(result[10]);
+			browscap4jBean.setMajorVersion(result[11]);
+			browscap4jBean.setMinorVersion(result[12]);
+			browscap4jBean.setPlatform(result[13]);
+			browscap4jBean.setPlatformVersion(result[14]);
+			browscap4jBean.setPlatformDescription(result[15]);
+			browscap4jBean.setPlatformBits(result[16]);
+			browscap4jBean.setPlatformMaker(result[17]);
+			browscap4jBean.setAlpha(result[18]);
+			browscap4jBean.setBeta(result[19]);
+			browscap4jBean.setWin16(result[20]);
+			browscap4jBean.setWin32(result[21]);
+			browscap4jBean.setWin64(result[22]);
+			browscap4jBean.setFrames(result[23]);
+			browscap4jBean.setiFrames(result[24]);
+			browscap4jBean.setTables(result[25]);
+			browscap4jBean.setCookies(result[26]);
+			browscap4jBean.setBackgroundSounds(result[27]);
+			browscap4jBean.setJavaScript(result[28]);
+			browscap4jBean.setVbScript(result[29]);
+			browscap4jBean.setJavaApplets(result[30]);
+			browscap4jBean.setActiveXControls(result[31]);
+			browscap4jBean.setIsMobileDevice(result[32]);
+			browscap4jBean.setIsTablet(result[33]);
+			browscap4jBean.setIsSyndicationReader(result[34]);
+			browscap4jBean.setCrawler(result[35]);
+			browscap4jBean.setIsFake(result[36]);
+			browscap4jBean.setIsAnonymized(result[37]);
+			browscap4jBean.setIsModified(result[38]);
+			browscap4jBean.setCssVersion(result[39]);
+			browscap4jBean.setAolVersion(result[40]);
+			browscap4jBean.setDeviceName(result[41]);
+			browscap4jBean.setDeviceMaker(result[42]);
+			browscap4jBean.setDeviceType(result[43]);
+			browscap4jBean.setDevicePointingMethod(result[44]);
+			browscap4jBean.setDeviceCodeName(result[45]);
+			browscap4jBean.setDeviceBrandName(result[46]);
+			browscap4jBean.setRenderingEngineName(result[47]);
+			browscap4jBean.setRenderingEngineVersion(result[48]);
+			browscap4jBean.setRenderingEngineDescription(result[49]);
+			browscap4jBean.setRenderingEngineMaker(result[50].substring(0, result[50].length() - 1));
 
 			log.debug("Browsercapabilities were found for      : " + userAgentString);
 			log.debug("*browsecap4jMapEntry.get().getKey()     : " + browsecap4jMapEntry.get().getKey());
-			log.debug("*browsecap4jMapEntry.get().getValue()   : " + browsecap4jMapEntry.get().getValue());
+			log.debug("*browsecap4jMapEntry.get()...getOffset(): " + browsecap4jMapEntry.get().getValue().getOffset());
+			log.debug("*browsecap4jMapEntry.get()...getLength(): " + browsecap4jMapEntry.get().getValue().getLength());
+
 		} else {
 			browscap4jBean.init();
 
@@ -277,7 +292,6 @@ public class Browscap4jFileReader {
 		browscap4jBean.setUserAgentStringRegEx(userAgentStringRegEx);
 
 		return browscap4jBean;
-
 	}
 
 	/**
@@ -321,7 +335,6 @@ public class Browscap4jFileReader {
 		stringBuffer.append("$");
 
 		return stringBuffer.toString().toLowerCase();
-
 	}
 
 }
